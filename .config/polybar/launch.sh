65e4aa8f7168bb3aa5890f477f3766443ff9e937
lwidm <lukas@widmer.id.au>
2024-04-20 10:46:50 +0200

    added mocha theme to i3 and example polybar bar

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# If all your bars have ipc enabled, you can use
+polybar-msg cmd quit
+# Otherwise you can use the nuclear option:
+# killall -q polybar
+
+# Luanch bar1 and bar2
+echo "---" | tee /tmp/polybarExample.log 
+polybar example 2>&1 | tee -a /tmp/polybarExample.log & disown
+
+echo "Bars launched..."
