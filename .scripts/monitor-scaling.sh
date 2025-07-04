7bba21dbdbc0c5e1ebde53b773393077c4f088d3
lwidm <lukas@widmer.id.au>
2025-07-04 10:26:15 +0200

    switched to hdmi

--- .scripts/monitor-scaling.sh
+++ .scripts/monitor-scaling.sh
@@ -6,7 +6,8 @@ if [[ "$MYSYSTEM" == "DebianDesktop" || "$MYSYSTEM" == "DebDesktop" ]]; then
     # Monitor identifiers
     MON1="DP-2.8"       # First 1440p monitor
     MON2="DP-0"     # Second 1440p monitor (to be rotated)
-    MON4K="DP-4.8"        # 4K monitor
+    # MON4K="DP-4.8"        # 4K monitor
+    MON4K="HDMI-0"        # 4K monitor
 
     # Calculate positions
     POS1="0x560"         # First monitor position
