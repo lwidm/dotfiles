cac20bf407d8ad304a51fdc76704051e511b781b
lwidm <lukas@widmer.id.au>
2026-03-06 20:07:27 +0100

    added more symbols for tuned

--- .config/eww/scripts/tuned_info.sh
+++ .config/eww/scripts/tuned_info.sh
@@ -3,12 +3,29 @@
 profile=$(cat /etc/tuned/active_profile 2>/dev/null || echo "unknown")
 
 case "$profile" in
-  "desktop")                icon="󰍹" ;;
-  "powersave")              icon="󰌪" ;;
-  "balanced-battery")       icon="󰾆" ;;
-  "balanced")               icon="󰾅" ;;
-  "throughput-performance") icon="󰓅" ;;
   *)                        icon="󰘚" ;;
+esac
+
+case "$profile" in
+  "accelerator-performance")    icon="" ;;
+  "aws")                        icon="" ;;
+  "balanced")                   icon="󰾅" ;;
+  "balanced-battery")           icon="󰾆" ;;
+  "cpu-partitioning")           icon="" ;;
+  "cpu-partitioning-powersave") icon="" ;;
+  "desktop")                    icon="󰍹" ;;
+  "hpc-compute")                icon="" ;;
+  "intel-sst")                  icon="" ;;
+  "latency-performance")        icon="" ;;
+  "mssql")                      icon="" ;;
+  "network-latency")            icon="" ;;
+  "network-throughput")         icon="" ;;
+  "optimize-serial-console")    icon="" ;;
+  "powersave")                  icon="󰌪" ;;
+  "throughput-performance")     icon="󰓅" ;;
+  "virtual-guest")              icon="" ;;
+  "virtual-host")               icon="" ;;
+  *)                            icon="unknown tuned profile!󰘚" ;;
 esac
 
 echo "{\"profile\": \"$profile\", \"icon\": \"$icon\"}"
